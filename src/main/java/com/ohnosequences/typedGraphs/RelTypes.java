59d16e53b5f1b791e18ab1a66ee33c2f32396153
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2014-05-08 12:50:44 +0200

    add all possible rel types

--- src/main/java/com/ohnosequences/typedGraphs/RelTypes.java
+++ src/main/java/com/ohnosequences/typedGraphs/RelTypes.java
@@ -2,61 +2,101 @@ package com.ohnosequences.typedGraphs;
 
 public interface RelTypes {
 
+  // Bounds over targets
   interface ToMany <
-  S extends Node<S,ST>,
-  ST extends Enum<ST> & NodeType<S,ST>,
-  R extends Relationship<S,ST,R,RT,T,TT>, 
-  RT extends Enum<RT> & RelationshipType<S,ST,R,RT,T,TT>,
-  T extends Node<T,TT>,
-  TT extends Enum<TT> & NodeType<T,TT>
-> extends RelationshipType<S,ST, R,RT, T,TT> 
-{}
+    S extends Node<S,ST>,
+    ST extends Enum<ST> & NodeType<S,ST>,
+    R extends Relationship<S,ST,R,RT,T,TT>, 
+    RT extends Enum<RT> & RelationshipType<S,ST,R,RT,T,TT>,
+    T extends Node<T,TT>,
+    TT extends Enum<TT> & NodeType<T,TT>
+  > extends RelationshipType<S,ST, R,RT, T,TT> 
+  {}
 
-interface ToOne <
-  S extends Node<S,ST>,
-  ST extends Enum<ST> & NodeType<S,ST>,
-  R extends Relationship<S,ST,R,RT,T,TT>, 
-  RT extends Enum<RT> & RelationshipType<S,ST,R,RT,T,TT>,
-  T extends Node<T,TT>,
-  TT extends Enum<TT> & NodeType<T,TT>
-> extends RelationshipType<S,ST, R,RT, T,TT> 
-{}
+  interface ToOne <
+    S extends Node<S,ST>,
+    ST extends Enum<ST> & NodeType<S,ST>,
+    R extends Relationship<S,ST,R,RT,T,TT>, 
+    RT extends Enum<RT> & RelationshipType<S,ST,R,RT,T,TT>,
+    T extends Node<T,TT>,
+    TT extends Enum<TT> & NodeType<T,TT>
+  > extends RelationshipType<S,ST, R,RT, T,TT> 
+  {}
 
-interface FromMany <
-  S extends Node<S,ST>,
-  ST extends Enum<ST> & NodeType<S,ST>,
-  R extends Relationship<S,ST,R,RT,T,TT>, 
-  RT extends Enum<RT> & RelationshipType<S,ST,R,RT,T,TT>,
-  T extends Node<T,TT>,
-  TT extends Enum<TT> & NodeType<T,TT>
-> extends RelationshipType<S,ST, R,RT, T,TT> 
-{}
+  // Bounds over sources
+  interface FromMany <
+    S extends Node<S,ST>,
+    ST extends Enum<ST> & NodeType<S,ST>,
+    R extends Relationship<S,ST,R,RT,T,TT>, 
+    RT extends Enum<RT> & RelationshipType<S,ST,R,RT,T,TT>,
+    T extends Node<T,TT>,
+    TT extends Enum<TT> & NodeType<T,TT>
+  > extends RelationshipType<S,ST, R,RT, T,TT> 
+  {}
 
-interface FromOne <
-  S extends Node<S,ST>,
-  ST extends Enum<ST> & NodeType<S,ST>,
-  R extends Relationship<S,ST,R,RT,T,TT>, 
-  RT extends Enum<RT> & RelationshipType<S,ST,R,RT,T,TT>,
-  T extends Node<T,TT>,
-  TT extends Enum<TT> & NodeType<T,TT>
-> extends RelationshipType<S,ST, R,RT, T,TT> 
-{}
+  interface FromOne <
+    S extends Node<S,ST>,
+    ST extends Enum<ST> & NodeType<S,ST>,
+    R extends Relationship<S,ST,R,RT,T,TT>, 
+    RT extends Enum<RT> & RelationshipType<S,ST,R,RT,T,TT>,
+    T extends Node<T,TT>,
+    TT extends Enum<TT> & NodeType<T,TT>
+  > extends RelationshipType<S,ST, R,RT, T,TT> 
+  {}
 
 
+  // all the possible cases, just for convenience
+  interface OneToMany <
+    S extends Node<S,ST>,
+    ST extends Enum<ST> & NodeType<S,ST>,
+    R extends Relationship<S,ST,R,RT,T,TT>, 
+    RT extends Enum<RT> & RelationshipType<S,ST,R,RT,T,TT>,
+    T extends Node<T,TT>,
+    TT extends Enum<TT> & NodeType<T,TT>
+  > extends FromOne<S,ST, R,RT, T,TT>, ToMany<S,ST, R,RT, T,TT> 
+  {
 
-interface OneToMany <
-  S extends Node<S,ST>,
-  ST extends Enum<ST> & NodeType<S,ST>,
-  R extends Relationship<S,ST,R,RT,T,TT>, 
-  RT extends Enum<RT> & RelationshipType<S,ST,R,RT,T,TT>,
-  T extends Node<T,TT>,
-  TT extends Enum<TT> & NodeType<T,TT>
-> extends FromOne<S,ST, R,RT, T,TT>, ToMany<S,ST, R,RT, T,TT> 
-{
+    public default Arity arity() { return Arity.oneToMany; }
+  }
 
-  public default Arity arity() { return Arity.oneToMany; }
-}
+  interface OneToOne <
+    S extends Node<S,ST>,
+    ST extends Enum<ST> & NodeType<S,ST>,
+    R extends Relationship<S,ST,R,RT,T,TT>, 
+    RT extends Enum<RT> & RelationshipType<S,ST,R,RT,T,TT>,
+    T extends Node<T,TT>,
+    TT extends Enum<TT> & NodeType<T,TT>
+  > extends FromOne<S,ST, R,RT, T,TT>, ToOne<S,ST, R,RT, T,TT> 
+  {
 
+    public default Arity arity() { return Arity.oneToOne; }
+  }
+
+  interface ManyToOne <
+    S extends Node<S,ST>,
+    ST extends Enum<ST> & NodeType<S,ST>,
+    R extends Relationship<S,ST,R,RT,T,TT>, 
+    RT extends Enum<RT> & RelationshipType<S,ST,R,RT,T,TT>,
+    T extends Node<T,TT>,
+    TT extends Enum<TT> & NodeType<T,TT>
+  > extends FromMany<S,ST, R,RT, T,TT>, ToOne<S,ST, R,RT, T,TT> 
+  {
+
+    public default Arity arity() { return Arity.manyToOne; }
+  }
+
+  interface ManyToMany <
+    S extends Node<S,ST>,
+    ST extends Enum<ST> & NodeType<S,ST>,
+    R extends Relationship<S,ST,R,RT,T,TT>, 
+    RT extends Enum<RT> & RelationshipType<S,ST,R,RT,T,TT>,
+    T extends Node<T,TT>,
+    TT extends Enum<TT> & NodeType<T,TT>
+  > extends FromMany<S,ST, R,RT, T,TT>, ToMany<S,ST, R,RT, T,TT> 
+  {
+
+    public default Arity arity() { return Arity.manyToMany; }
+  }
 
 }
 
