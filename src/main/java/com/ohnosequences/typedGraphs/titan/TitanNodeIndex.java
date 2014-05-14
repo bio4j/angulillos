e4ba60486d4aaa9f144f13b94f5fe51a83c62ad6
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2014-05-14 21:44:41 +0200

    default list titan index

--- src/main/java/com/ohnosequences/typedGraphs/titan/TitanNodeIndex.java
+++ src/main/java/com/ohnosequences/typedGraphs/titan/TitanNodeIndex.java
@@ -6,6 +6,9 @@ import com.thinkaurelius.titan.core.attribute.Cmp;
 import com.thinkaurelius.titan.core.*;
 
 import java.util.List;
+import java.util.LinkedList;
+import java.util.Iterator;
+import com.tinkerpop.blueprints.Vertex;
 
 public interface TitanNodeIndex <
   N extends Node<N,NT>,
@@ -58,7 +61,7 @@ public interface TitanNodeIndex <
     public java.util.List<? extends Node<N,NT>> getNodes(V byValue);
   }
 
-  class Default <
+  class DefaultUnique <
     N extends Node<N,NT>,
     NT extends Enum<NT> & NodeType<N,NT>,
 
@@ -70,6 +73,12 @@ public interface TitanNodeIndex <
     V
   > implements Unique<N,NT,TitanN,TitanNT,P,PT,V> {
 
+    public DefaultUnique(TitanTypedGraph graph, TitanNodeType<N,NT, TitanN,TitanNT> nodeType) {
+
+      this.graph = graph;
+      this.nodeType = nodeType;
+    }
+
     private TitanTypedGraph graph;
     private TitanNodeType<N,NT, TitanN,TitanNT> nodeType;
 
@@ -79,6 +88,42 @@ public interface TitanNodeIndex <
       TitanVertex uglyStuff = (TitanVertex) graph.rawGraph.query().has(nodeType.titanKey().getName(),Cmp.EQUAL,byValue).vertices().iterator().next();
 
       return nodeType.from(uglyStuff);
+    }
+
+  }
+
+  class DefaultList <
+    N extends Node<N,NT>,
+    NT extends Enum<NT> & NodeType<N,NT>,
+
+    TitanN extends TitanNode<N,NT, TitanN,TitanNT>,
+    TitanNT extends Enum<TitanNT> & TitanNodeType<N,NT, TitanN,TitanNT>,
+
+    P extends Property<N,NT>,
+    PT extends PropertyType<N,NT, P,PT, V>, 
+    V
+  > implements List<N,NT,TitanN,TitanNT,P,PT,V> {
+
+    public DefaultList(TitanTypedGraph graph, TitanNodeType<N,NT, TitanN,TitanNT> nodeType) {
+
+      this.graph = graph;
+      this.nodeType = nodeType;
+    }
+
+    private TitanTypedGraph graph;
+    private TitanNodeType<N,NT, TitanN,TitanNT> nodeType;
+
+    public java.util.List<Node<N,NT>> getNodes(V byValue) {
+
+      java.util.List<Node<N,NT>> list = new LinkedList<>();
+      Iterator<Vertex> iterator = graph.rawGraph.query().has(nodeType.titanKey().getName(),Cmp.EQUAL,byValue).vertices().iterator();
+      
+      while (iterator.hasNext()) {
+
+        list.add(nodeType.from( (TitanVertex) iterator.next() ));
+      }
+
+      return list;
     }
 
   }
