5b7568d11cfe9a6350e1b4009a4c1aa7809d1fa0
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2014-05-19 10:23:09 -0400

    properties should be interfaces

--- src/main/java/com/ohnosequences/typedGraphs/Property.java
+++ src/main/java/com/ohnosequences/typedGraphs/Property.java
@@ -6,7 +6,7 @@ package com.ohnosequences.typedGraphs;
   @author <a href="mailto:eparejatobes@ohnosequences.com">Eduardo Pareja-Tobes</a>
 */
 
-public abstract class Property <
+public interface Property <
   // the element type
   N extends Element<N,NT>, NT extends Element.Type<N,NT>,
   // the property type
@@ -15,29 +15,15 @@ public abstract class Property <
   V
 > 
 {
+   /*
+    the element type which has this property type
+  */
+  public NT elementType();
+  public Class<V> valueClass();
+  public String name();
     
-  protected Property(NT elementType, String name, Class<V> clazz) {
-
-    this.elementType = elementType;
-    this.name = name;
-    this.clazz = clazz;
-  }
-
-  private NT elementType;
-  private String name;
-  private Class<V> clazz;
-
   /*
     the name is by default the name of the element together with that of the unique value here
   */
-  public String fullName() { return elementType.name().concat(".").concat(name); }
-
-  /*
-    the element type which has this property type
-  */
-  public NT elementType() { return this.elementType; }
-
-  // just in case
-  public Class<V> valueClass() { return this.clazz; }
-  public String name() { return this.name; }
+  public default String fullName() { return elementType().name().concat(".").concat(name()); }
 }
--- src/main/java/com/ohnosequences/typedGraphs/titan/TitanProperty.java
+++ src/main/java/com/ohnosequences/typedGraphs/titan/TitanProperty.java
@@ -9,28 +9,19 @@ import com.thinkaurelius.titan.core.TitanVertex;
 import com.thinkaurelius.titan.core.TitanKey;
 import com.thinkaurelius.titan.core.TitanElement;
 
-public abstract class TitanProperty <
+public interface TitanProperty <
   // the element type
   N extends Element<N,NT>, NT extends Element.Type<N,NT>,
   // the property (of that element)
-  P extends Property<N,NT,P,V>,
+  P extends TitanProperty<N,NT,P,V>,
   // the value type of this property
   V
 > 
   extends Property<N,NT,P,V>
 {
 
-  protected TitanProperty(NT elementType, String name, Class<V> clazz, TitanKey titanKey) {
-
-    super(elementType,name,clazz);
-
-    this.titanKey = titanKey;
-  }
-
   /*
     The Titan key used for this property type
   */
-  protected TitanKey titanKey;
-
-  public TitanKey titanKey() { return titanKey; }
+  public TitanKey titanKey();
 }
