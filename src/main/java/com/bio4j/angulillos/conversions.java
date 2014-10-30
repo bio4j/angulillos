70ad73321f66aefc9d09fb6694517a2ebef1d63a
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2014-10-30 16:06:09 +0100

    naive implementation, also added some more stream builders

--- src/main/java/com/bio4j/angulillos/conversions.java
+++ src/main/java/com/bio4j/angulillos/conversions.java
@@ -3,10 +3,41 @@ package com.bio4j.angulillos;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
+import java.util.Optional;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.Iterator;
+
 public class conversions {
   
   public static <O> Stream<O> stream(Iterable<O> iterable) {
 
-    return StreamSupport.stream(iterable.spliterator(), false);
+    return stream(iterable.spliterator());
+  }
+
+  public static <O> Stream<O> stream(Spliterator<O> spliterator) {
+
+    return StreamSupport.stream(spliterator, false);
+  }
+
+  public static <O> Stream<O> stream(Iterator<O> iterator) {
+
+    return stream(Spliterators.spliteratorUnknownSize(iterator, Spliterator.ORDERED));
+  }
+
+  public static <O> Optional<Stream<O>> any(Stream<Optional<O>> stream) {
+
+    Stream<O> filtered = stream.filter(Optional::isPresent).map(Optional::get);
+
+    Iterator<O> it_filtered = filtered.iterator();
+
+    if ( it_filtered.hasNext() ) {
+
+      return Optional.of(stream(it_filtered));
+
+    } else {
+
+      return Optional.empty();
+    }
   }
 }
