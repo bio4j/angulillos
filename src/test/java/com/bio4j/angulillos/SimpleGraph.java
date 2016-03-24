a88275fe579a9fa6151b5e9dd2edeaa1cbdfb9b5
Alexey Alekhin <laughedelic@gmail.com>
2016-03-24 20:53:19 +0100

    Added (private) Element class to SimpleGraph to reduce the boilerplate in Vertex and Edge classes

--- src/test/java/com/bio4j/angulillos/SimpleGraph.java
+++ src/test/java/com/bio4j/angulillos/SimpleGraph.java
@@ -3,47 +3,48 @@ package com.bio4j.angulillos;
 // import com.bio4j.angulillos.*;
 
 public abstract class SimpleGraph<
-  SG extends SimpleGraph<SG,RG, RV,RE>,
-  RG extends UntypedGraph<RV,RE>,
+  SG extends SimpleGraph<SG,RV,RE>,
   RV,RE
-> implements
-  TypedGraph<SG,RV,RE>
-{
+> implements TypedGraph<SG,RV,RE> {
 
   public abstract SG self();
 
-  protected RG raw;
-  @Override public RG raw() { return this.raw; }
+  private final UntypedGraph<RV,RE> raw;
+  @Override public final UntypedGraph<RV,RE> raw() { return this.raw; }
 
-  protected SimpleGraph(RG raw) { this.raw = raw; }
+  protected SimpleGraph(UntypedGraph<RV,RE> raw) { this.raw = raw; }
 
   /* ### Abstract helper classes */
 
-  // public abstract class ElementType<
-  //   E  extends     SG.Element<E, ET>,
-  //   ET extends SG.ElementType<E, ET>
-  // > implements
-  //   TypedElement.Type<E,ET,SG, I, RV,RE>
-  // {
-  //   @Override public final SG graph() { return SimpleGraph.self(); }
-  // }
-  //
-  // public abstract class Element<
-  //   E extends SG.Element<E,ET>,
-  //   ET extends SG.ElementType<E,ET>
-  // >
-  // implements
-  //   TypedElement<E,ET,SG, I, RV,RE>
-  // {
-  //   @Override public final SG graph() { return SimpleGraph.self(); }
-  // }
+  private abstract class Element<
+    F  extends           Element<F,FT, RF>,
+    FT extends TypedElement.Type<F,FT, SG,RF>,
+    RF
+  > implements TypedElement<F,FT, SG,RF> {
+
+    @Override public final SG graph() { return SimpleGraph.this.self(); }
+
+    private final RF raw;
+    private final FT type;
+
+    @Override public final RF raw()  { return this.raw; }
+    @Override public final FT type() { return this.type; }
+
+    // NOTE: we cannot do the same to `self`, because `super()` constructor cannot refer to `this`
+    protected Element(RF raw, FT type) {
+      this.raw  = raw;
+      this.type = type;
+    }
+  }
+
+  // NOTE: there is no point in a class for TypedElement.Type, because there is nothing that we can implement
+
 
   public class Property<
     FT extends TypedElement.Type<?,FT, SG,?>,
     X
-  > implements
-    com.bio4j.angulillos.Property<FT,X>
-  {
+  > implements com.bio4j.angulillos.Property<FT,X> {
+
     private final FT elementType;
     private final Class<X> valueClass;
 
@@ -59,22 +60,10 @@ public abstract class SimpleGraph<
   public abstract class Vertex<
     V  extends     Vertex<V,VT>,
     VT extends VertexType<V,VT>
-  > implements
-    TypedVertex<V,VT, SG,RV,RE>
-  {
-    @Override public final SG graph() { return SimpleGraph.this.self(); }
+  > extends Element<V,VT, RV>
+    implements TypedVertex<V,VT, SG,RV,RE> {
 
-    private final RV raw;
-    private final VT type;
-
-    @Override public final RV raw()  { return this.raw; }
-    @Override public final VT type() { return this.type; }
-
-    // NOTE: we cannot do the same to self, because `super()` constructor cannot refer to `this`
-    protected Vertex(RV raw, VT type) {
-      this.raw  = raw;
-      this.type = type;
-    }
+    protected Vertex(RV raw, VT type) { super(raw, type); }
   }
 
   public abstract class VertexType<
@@ -91,21 +80,10 @@ public abstract class SimpleGraph<
     ET extends EdgeType<S,ST, E,ET, T,TT>,
     T  extends     Vertex<T,TT>,
     TT extends VertexType<T,TT>
-  > implements
-    com.bio4j.angulillos.TypedEdge<S,ST, E,ET, T,TT, SG,RV,RE>
-  {
-    @Override public final SG graph() { return SimpleGraph.this.self(); }
+  > extends Element<E,ET, RE>
+    implements TypedEdge<S,ST, E,ET, T,TT, SG,RV,RE> {
 
-    private final RE edge;
-    private final ET type;
-
-    protected Edge(RE edge, ET type) {
-      this.edge = edge;
-      this.type = type;
-    }
-
-    @Override public final RE raw() { return this.edge; }
-    @Override public final ET type() { return type; }
+    protected Edge(RE raw, ET type) { super(raw, type); }
   }
 
   public abstract class EdgeType<
