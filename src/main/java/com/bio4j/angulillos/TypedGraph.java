f17a4e75e505b784884c60d2a4f1875fd70d8b9f
Alexey Alekhin <laughedelic@gmail.com>
2016-03-29 01:59:03 +0200

    Just moved things from GraphSchema to TypedGraph (now it's one class)

--- src/main/java/com/bio4j/angulillos/TypedGraph.java
+++ src/main/java/com/bio4j/angulillos/TypedGraph.java
@@ -11,12 +11,42 @@ import java.util.Optional;
 
   A `TypedGraph` is, unsurprisingly, the typed version of [UntypedGraph](UntypedGraph.java.md).
 */
-interface TypedGraph <
+public abstract class TypedGraph <
   G extends TypedGraph<G,RV,RE>,
   RV,RE
->
-{
+> {
 
-  UntypedGraph<RV,RE> raw();
+  public abstract G self();
+
+  private final UntypedGraph<RV,RE> raw;
+  public  final UntypedGraph<RV,RE> raw() { return this.raw; }
+
+  protected TypedGraph(UntypedGraph<RV,RE> raw) { this.raw = raw; }
+
+
+  /* Refined graph element classes */
+
+  public abstract class VertexType<
+    VT extends VertexType<VT>
+  > extends com.bio4j.angulillos.VertexType<VT,G,RV,RE> {
+
+    @Override public final G graph() { return TypedGraph.this.self(); }
+  }
+
+  public abstract class EdgeType<
+    // source
+    ST extends com.bio4j.angulillos.VertexType<ST, SG,RV,RE>,
+    SG extends TypedGraph<SG,RV,RE>,
+    // edge itself
+    ET extends EdgeType<ST,SG, ET, TT,TG>,
+    // target
+    TT extends com.bio4j.angulillos.VertexType<TT, TG,RV,RE>,
+    TG extends TypedGraph<TG,RV,RE>
+  > extends com.bio4j.angulillos.EdgeType<ST,SG, ET,G, TT,TG, RV,RE> {
+
+    protected EdgeType(ST sourceType, TT targetType) { super(sourceType, targetType); }
+
+    @Override public final G graph() { return TypedGraph.this.self(); }
+  }
 
 }
