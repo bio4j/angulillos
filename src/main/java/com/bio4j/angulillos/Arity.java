a945705e1dcc65abe24684e969782f5d401de2cf
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2016-05-13 17:17:19 +0200

    make Arity interface hierarchy match their view as predicates

--- src/main/java/com/bio4j/angulillos/Arity.java
+++ src/main/java/com/bio4j/angulillos/Arity.java
@@ -11,16 +11,15 @@ public enum Arity {
   AtLeastOne, // Non-empty list: NEList[X]
   Any;        // Usual list: List[X]
 
-
   /* #### In-arities */
-  public interface FromAtLeastOne extends HasFromArity { default Arity fromArity() { return Arity.One; } }
-  public interface FromOne        extends HasFromArity { default Arity fromArity() { return Arity.AtMostOne; } }
-  public interface FromAtMostOne  extends HasFromArity { default Arity fromArity() { return Arity.AtLeastOne; } }
+  public interface FromAtLeastOne extends FromAny, HasFromArity { default Arity fromArity() { return Arity.One; } }
+  public interface FromOne        extends FromAtLeastOne, FromAtMostOne, HasFromArity { default Arity fromArity() { return Arity.AtMostOne; } }
+  public interface FromAtMostOne  extends FromAny, HasFromArity { default Arity fromArity() { return Arity.AtLeastOne; } }
   public interface FromAny        extends HasFromArity { default Arity fromArity() { return Arity.Any; } }
 
   /* #### Out-arities */
-  public interface ToAtLeastOne extends HasToArity { default Arity toArity() { return Arity.One; } }
-  public interface ToOne        extends HasToArity { default Arity toArity() { return Arity.AtMostOne; } }
-  public interface ToAtMostOne  extends HasToArity { default Arity toArity() { return Arity.AtLeastOne; } }
+  public interface ToAtLeastOne extends ToAny, HasToArity { default Arity toArity() { return Arity.One; } }
+  public interface ToOne        extends ToAtLeastOne, ToAtMostOne, HasToArity { default Arity toArity() { return Arity.AtMostOne; } }
+  public interface ToAtMostOne  extends ToAny, HasToArity { default Arity toArity() { return Arity.AtLeastOne; } }
   public interface ToAny        extends HasToArity { default Arity toArity() { return Arity.Any; } }
 }
