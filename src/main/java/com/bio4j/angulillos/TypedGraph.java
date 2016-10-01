3fc6c3f0a5cf85550b85c748ab9e5e3d419d6928
Alexey Alekhin <laughedelic@gmail.com>
2016-10-01 02:47:02 +0200

    Removed TypedLinkGraph and instead added generic edges to the TypedGraph with simple edge just being their instances

--- src/main/java/com/bio4j/angulillos/TypedGraph.java
+++ src/main/java/com/bio4j/angulillos/TypedGraph.java
@@ -209,17 +209,131 @@ public abstract class TypedGraph<
     }
   }
 
+  public abstract class GenericEdge<
+    SG extends TypedGraph<SG,RV,RE>,
+    S  extends TypedGraph<SG,RV,RE>.Vertex<S>,
+    E  extends GenericEdge<SG,S, E, TG,T>,
+    TG extends TypedGraph<TG,RV,RE>,
+    T  extends TypedGraph<TG,RV,RE>.Vertex<T>
+  > extends Element<E, GenericEdgeType<SG,S, E, TG,T>, RE>
+    implements TypedEdge<
+      S, TypedGraph<SG,RV,RE>.VertexType<S>,
+      E, GenericEdgeType<SG,S, E, TG,T>,
+      T, TypedGraph<TG,RV,RE>.VertexType<T>,
+      G,RV,RE
+    > {
+
+      protected GenericEdge(RE raw, GenericEdgeType<SG,S, E, TG,T> type) { super(raw, type); }
+    }
+
+  public abstract class GenericEdgeType<
+    SG extends TypedGraph<SG,RV,RE>,
+    S  extends TypedGraph<SG,RV,RE>.Vertex<S>,
+    E  extends GenericEdge<SG,S, E, TG,T>,
+    TG extends TypedGraph<TG,RV,RE>,
+    T  extends TypedGraph<TG,RV,RE>.Vertex<T>
+  > extends ElementType<E, GenericEdgeType<SG,S, E, TG,T>, RE>
+    implements TypedEdge.Type<
+      S, TypedGraph<SG,RV,RE>.VertexType<S>,
+      E, GenericEdgeType<SG,S, E, TG,T>,
+      T, TypedGraph<TG,RV,RE>.VertexType<T>,
+      G,RV,RE
+    > {
+
+      protected GenericEdgeType<SG,S, E, TG,T> self() { return this; }
+
+      private final TypedGraph<SG,RV,RE>.VertexType<S> sourceType;
+      private final TypedGraph<TG,RV,RE>.VertexType<T> targetType;
+
+      @Override public final TypedGraph<SG,RV,RE>.VertexType<S> sourceType() { return this.sourceType; }
+      @Override public final TypedGraph<TG,RV,RE>.VertexType<T> targetType() { return this.targetType; }
+
+      // NOTE: this initializer block will be inherited and will add each edge type to the set
+      {
+        if (
+          TypedGraph.this.edgeTypes.removeIf( (AnyEdgeType et) ->
+            et._label().equals( self()._label() )
+          )
+        ) {
+          throw new IllegalArgumentException("The graph contains duplicate edge type: " + self()._label());
+        }
+
+        TypedGraph.this.edgeTypes.add(self());
+      }
+
+      protected GenericEdgeType(
+        TypedGraph<SG,RV,RE>.VertexType<S> sourceType,
+        TypedGraph<TG,RV,RE>.VertexType<T> targetType
+      ) {
+
+        this.sourceType = sourceType;
+        this.targetType = targetType;
+
+        sourceType.outEdges.add( self() );
+        targetType.inEdges.add( self() );
+      }
+
+      public class UniqueIndex<
+        P extends Property<X> & Arity.FromAtMostOne,
+        X
+      >
+      implements com.bio4j.angulillos.TypedEdgeIndex.Unique<E,GenericEdgeType<SG,S, E, TG,T>,P,X,RV,RE> {
+
+        private final P property;
+        public final P property() { return property; }
+
+        protected UniqueIndex(P property) { this.property = property; }
+
+        {
+          if(
+            TypedGraph.this.uniqueEdgeIndexes.removeIf(
+              vt -> vt._label().equals( _label() )
+            )
+          )
+          {
+            throw new IllegalArgumentException("The graph contains a duplicate index type: " + _label());
+          }
+          else {
+
+            TypedGraph.this.uniqueEdgeIndexes.add( this );
+          }
+        }
+      }
+
+      public class NonUniqueIndex<
+        P extends Property<X>,
+        X
+      >
+      implements com.bio4j.angulillos.TypedEdgeIndex.NonUnique<E,GenericEdgeType<SG,S, E, TG,T>,P,X,RV,RE> {
+
+        private final P property;
+        public final P property() { return property; }
+
+        protected NonUniqueIndex(P property) { this.property = property; }
+
+        {
+          if(
+            TypedGraph.this.nonUniqueEdgeIndexes.removeIf(
+              vt -> vt._label().equals( _label() )
+            )
+          )
+          {
+            throw new IllegalArgumentException("The graph contains a duplicate index type: " + _label());
+          }
+          else {
+
+            TypedGraph.this.nonUniqueEdgeIndexes.add( this );
+          }
+        }
+      }
+    }
+
+
   public abstract class Edge<
     S extends Vertex<S>,
     E extends Edge<S,E,T>,
     T extends Vertex<T>
-  > extends Element<E, EdgeType<S,E,T>, RE>
-    implements TypedEdge<
-      S, VertexType<S>,
-      E, EdgeType<S,E,T>,
-      T, VertexType<T>,
-      G,RV,RE
-    > {
+    > extends GenericEdge<G,S, E, G,T> {
 
     protected Edge(RE raw, EdgeType<S,E,T> type) { super(raw, type); }
   }
@@ -228,97 +342,8 @@ public abstract class TypedGraph<
     S extends Vertex<S>,
     E extends Edge<S,E,T>,
     T extends Vertex<T>
-  > extends ElementType<E, EdgeType<S,E,T>, RE>
-    implements TypedEdge.Type<
-      S, VertexType<S>,
-      E, EdgeType<S,E,T>,
-      T, VertexType<T>,
-      G,RV,RE
-  >
-  {
+  > extends GenericEdgeType<G,S, E, G,T> {
 
-    protected EdgeType<S,E,T> self() { return this; }
-
-    private final VertexType<S> sourceType;
-    private final VertexType<T> targetType;
-
-    @Override public final VertexType<S> sourceType() { return this.sourceType; }
-    @Override public final VertexType<T> targetType() { return this.targetType; }
-
-    // NOTE: this initializer block will be inherited and will add each edge type to the set
-    {
-      if (
-        TypedGraph.this.edgeTypes.removeIf( (AnyEdgeType et) ->
-          et._label().equals( self()._label() )
-        )
-      ) {
-        throw new IllegalArgumentException("The graph contains duplicate edge type: " + self()._label());
-      }
-
-      TypedGraph.this.edgeTypes.add(self());
-    }
-
-    protected EdgeType(VertexType<S> sourceType, VertexType<T> targetType) {
-
-      this.sourceType = sourceType;
-      this.targetType = targetType;
-
-      sourceType.outEdges.add( self() );
-      targetType.inEdges.add( self() );
-    }
-
-    public class UniqueIndex<
-      P extends Property<X> & Arity.FromAtMostOne,
-      X
-    >
-    implements com.bio4j.angulillos.TypedEdgeIndex.Unique<E,EdgeType<S,E,T>,P,X,RV,RE> {
-
-      private final P property;
-      public final P property() { return property; }
-
-      protected UniqueIndex(P property) { this.property = property; }
-
-      {
-        if(
-          TypedGraph.this.uniqueEdgeIndexes.removeIf(
-            vt -> vt._label().equals( _label() )
-          )
-        )
-        {
-          throw new IllegalArgumentException("The graph contains a duplicate index type: " + _label());
-        }
-        else {
-
-          TypedGraph.this.uniqueEdgeIndexes.add( this );
-        }
-      }
-    }
-
-    public class NonUniqueIndex<
-      P extends Property<X>,
-      X
-    >
-    implements com.bio4j.angulillos.TypedEdgeIndex.NonUnique<E,EdgeType<S,E,T>,P,X,RV,RE> {
-
-      private final P property;
-      public final P property() { return property; }
-
-      protected NonUniqueIndex(P property) { this.property = property; }
-
-      {
-        if(
-          TypedGraph.this.nonUniqueEdgeIndexes.removeIf(
-            vt -> vt._label().equals( _label() )
-          )
-        )
-        {
-          throw new IllegalArgumentException("The graph contains a duplicate index type: " + _label());
-        }
-        else {
-
-          TypedGraph.this.nonUniqueEdgeIndexes.add( this );
-        }
-      }
-    }
+    protected EdgeType(VertexType<S> sourceType, VertexType<T> targetType) { super(sourceType, targetType); }
   }
 }
